d0fffadf6ccfd3130890af6e48b6acd7b224546b
Philip Walton <philip@philipwalton.com>
2022-11-07 15:46:01 -0800

    Add a PendingBeacon experiment

--- worker/lib/experiments.js
+++ worker/lib/experiments.js
@@ -1,12 +1,20 @@
 /* global HTMLRewriter */
 
+// philipwalton.com:
+// eslint-disable-next-line max-len
+// const PENDING_BEACON_TOKEN = 'As/j5gJ50BNvCX2nrZLywnV5VGVEUWwbM5er761RvSNXVPg6VmPy7xufiqm5fRyzcVtuJr4fQwbrs7jDrDyewgAAAABaeyJvcmlnaW4iOiJodHRwczovL3BoaWxpcHdhbHRvbi5jb206NDQzIiwiZmVhdHVyZSI6IlBlbmRpbmdCZWFjb25BUEkiLCJleHBpcnkiOjE2NzgyMzM1OTl9';
+
+// localhost.philipwalton.dev:
+// eslint-disable-next-line max-len
+const PENDING_BEACON_TOKEN = 'AprzbjkBS+m0mGTBvQ6Dd3YmtEK97DbrAWOoLJXueMRSKg7mFbvmbupqhqU25XI8gDndVwOU/qcCFCfoOqPigQAAAABkeyJvcmlnaW4iOiJodHRwczovL2xvY2FsaG9zdC5waGlsaXB3YWx0b24uZGV2OjQ0MyIsImZlYXR1cmUiOiJQZW5kaW5nQmVhY29uQVBJIiwiZXhwaXJ5IjoxNjc4MjMzNTk5fQ==';
+
 const experiments = {
   pending_beacon: {
     range: [0, 0.5],
-    transform: (response) => {
+    transform: (res) => {
       return new HTMLRewriter()
         .on('head>script:first-of-type', new ExperimentScriptHandler())
-        .transform(response);
+        .transform(res);
     },
   },
 };
@@ -20,7 +28,8 @@ class ExperimentScriptHandler {
    * @param {Object} element
    */
   element(element) {
-    element.before(`<script>self.__x='pending_beacon'</script>`, {
+    // eslint-disable-next-line max-len
+    element.before(`<meta http-equiv="origin-trial" content="${PENDING_BEACON_TOKEN}"><script>self.__x='pending_beacon'</script>`, {
       html: true,
     });
   }
